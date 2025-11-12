71399eebdd76d4cf670b7ee6d56ba52b4d5439fd
Coromoto León <cleon@ull.edu.es>
2025-11-12 09:30:16 +0000

    clase problemas 12/11/2025

--- herencia/02.point3D.rb
+++ herencia/02.point3D.rb
@@ -78,4 +78,51 @@ p33d = p13d + p23d
 
 l3d = [p03d,p13d,p23d,p33d]
 
+# La CLASE de un objeto
+# Un objeto tiene una CLASE bien definida en Ruby
+# Esa CLASE nunca CAMBIARÁ durante el tiempo de vida del objeto
+
+puts "Pruebas de clase"
+puts "p1.class                     #{p1.class}"
+puts "p1.class == Point            #{p1.class == Point}"
+puts "p1.instance_of?Point         #{p1.instance_of?Point}"      # PREDICADO
+
+# equivalentes
+puts "Pruebas de clase equivalente"
+puts "p1.is_a?Point                #{p1.is_a?Point}"
+puts "p1.kind_of?Point             #{p1.kind_of?Point}"                                                                      
+puts "Point === p1                 #{Point === p1}"
+
+# superclases 
+puts "Pruebas de herencia"
+puts "p1.is_a?Object               #{p1.is_a?Object}"
+puts "p1.kind_of?Object            #{p1.kind_of?Object}"
+puts "Object === p1                #{Object === p1}"
+
+puts "p1.is_a?BasicObject          #{p1.is_a?BasicObject}"
+puts "p1.kind_of?BasicObject       #{p1.kind_of?BasicObject}"
+puts "BasicObject === p1           #{BasicObject === p1}"
+
+
+# ¿Es instancia de cualquier superclase?
+puts "¿Es instancia de cualquier superclase?"
+
+puts "p13d.class                   #{p13d.class}"
+puts "p13d.class == Point3D        #{p13d.class == Point3D}"
+puts "p13d.instance_of?Point3D     #{p13d.instance_of?Point3D}"      
+
+puts "p13d.is_a?Point3D            #{p13d.is_a?Point3D}"
+puts "p13d.kind_of?Point3D         #{p13d.kind_of?Point3D}"
+puts "p13d.is_a?Point              #{p13d.is_a?Point}"
+puts "p13d.is_a?Object             #{p13d.is_a?Object}"
+puts "p13d.is_a?BasicObject        #{p13d.is_a?BasicObject}"
+
+# La clase de un objeto no incluye herencia
+puts "La clase de un objeto no incluye herencia"
+puts "p1.instance_of?Object        #{p1.instance_of?Object}"
+puts "p1.instance_of?BasicObject   #{p1.instance_of?BasicObject}"
+puts "p13d.instance_of?Point       #{p13d.instance_of?Point}"
+puts "p13d.instance_of?Object      #{p13d.instance_of?Object}"
+puts "p13d.instance_of?BasicObject #{p13d.instance_of?BasicObject}"
+
 binding.pry
